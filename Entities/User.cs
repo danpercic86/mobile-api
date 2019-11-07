03a5e0d33b965f5d26ab958046a2936cc61cb8f5
Daniel Hort <daniel.hort@ligaac.ro>
2019-11-07 18:14:40 +0200

    Remove unnecessary fields from user

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,5 +1,6 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace itec_mobile_api_final.Entities
 {
@@ -7,6 +8,18 @@ namespace itec_mobile_api_final.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [NotMapped]
+        public override string PhoneNumber { get; set; }
+        [NotMapped]
+        public override bool PhoneNumberConfirmed { get; set; }
+        [NotMapped]
+        public override bool TwoFactorEnabled { get; set; }
+        [NotMapped]
+        public override DateTimeOffset? LockoutEnd { get; set; }
+        [NotMapped]
+        public override bool LockoutEnabled { get; set; }
+        [NotMapped]
+        public override int AccessFailedCount { get; set; }
 
         public User() : base() {}
         public User(string userName) : base(userName) {}
--- Migrations/20191107160910_remove_unnecessary_fields_from_user.Designer.cs
+++ Migrations/20191107160910_remove_unnecessary_fields_from_user.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using itec_mobile_api_final.Data;
 
 namespace itec_mobile_api_final.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191107160910_remove_unnecessary_fields_from_user")]
+    partial class remove_unnecessary_fields_from_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
