f941cf3c4f07e6ee20435a41d99577d9e9711343
Dan Percic <danpercic86@gmail.com>
2019-11-13 21:36:37 +0200

    updated cars

--- Cars/CarsController.cs
+++ Cars/CarsController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using itec_mobile_api_final.Data;
 using itec_mobile_api_final.Extensions;
 using itec_mobile_api_final.Helpers;
-using itec_mobile_api_final.Models.Requests;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -64,14 +63,16 @@ namespace itec_mobile_api_final.Cars
         [HttpPatch("{id}")]
         public async Task<IActionResult> Patch([FromBody] dynamic car1, [FromRoute] string id)
         {
-            var car = _carRepository.GetAsync(id).Result;
-            if (car is null) return NotFound();
+            var userId = HttpContext.GetCurrentUserId();
+            if (userId is null) return Unauthorized();
+            
+            var car = await _carRepository.GetAsync(id);
+            if (car is null || car.UserId != userId) return NotFound();
 
             car = ReflectionHelper.PatchObject(car, car1);
 
             await _carRepository.UpdateAsync(car);
             return Ok(car);
-            
         }
 
         [HttpDelete("{id}")]
